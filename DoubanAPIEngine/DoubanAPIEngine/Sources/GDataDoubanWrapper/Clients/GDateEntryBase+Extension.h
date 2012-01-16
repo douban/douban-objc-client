d041f62109b7d97db25f9e57f76203c50382a109
GUO Lin <guolin.mobi@gmail.com>
2012-01-16 15:43:41 +0800

    refact the code

---
+++ DoubanAPIEngine/DoubanAPIEngine/Sources/GDataDoubanWrapper/Clients/GDateEntryBase+Extension.h
@@ -0,0 +1,13 @@
+//
+//  DoubanObject.h
+//  DoubanAPIEngine
+//
+//  Created by Lin GUO on 1/16/12.
+//  Copyright (c) 2012 Douban Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DoubanObject : NSObject
+
+@end
---
+++ DoubanAPIEngine/DoubanAPIEngine/Sources/GDataDoubanWrapper/Clients/GDateEntryBase+Extension.m
@@ -0,0 +1,13 @@
+//
+//  DoubanObject.m
+//  DoubanAPIEngine
+//
+//  Created by Lin GUO on 1/16/12.
+//  Copyright (c) 2012 Douban Inc. All rights reserved.
+//
+
+#import "DoubanObject.h"
+
+@implementation DoubanObject
+
+@end
