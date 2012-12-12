2c71c3fb929f8f74288b12ba76bbbe76897f0225
GUO Lin <guolin.mobi@gmail.com>
2012-12-12 18:30:25 +0800

    重新生成 framework

--- DoubanAPIEngine/DoubanAPIEngine/Sources/Model2/Music/DOUMusic.m
+++ DoubanAPIEngine/DoubanAPIEngine/Sources/Model2/Music/DOUMusic.m
@@ -1,8 +1,8 @@
 //
-//  DOUMovie.m
+//  DOUMusic.m
 //  DoubanAPIEngine
 //
-//  Created by GUO Lin on 12/5/12.
+//  Created by Panglv on 12/5/12.
 //  Copyright (c) 2012 Douban Inc. All rights reserved.
 //
 
--- DoubanAPIEngine/DoubanAPIEngine/Sources/Model2/Music/DOUMusicArray.h
+++ DoubanAPIEngine/DoubanAPIEngine/Sources/Model2/Music/DOUMusicArray.h
@@ -1,8 +1,8 @@
 //
-//  DOUMovieArray.h
+//  DOUMusicArray.h
 //  DoubanAPIEngine
 //
-//  Created by GUO Lin on 12/5/12.
+//  Created by Panglv on 12/5/12.
 //  Copyright (c) 2012 Douban Inc. All rights reserved.
 //
 
