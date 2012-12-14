b9605491aeebe525a26912ccb32a11dd16edafa8
GUO Lin <guolin.mobi@gmail.com>
2012-12-14 14:11:51 +0800

    添加 DOUNotification

---
+++ DoubanAPIEngine/DoubanAPIEngine/Sources/Model2/Community/DOUNotificationArray.h
@@ -0,0 +1,15 @@
+//
+//  DouNotificationArray.h
+//  DoubanAPIEngine
+//
+//  Created by alex zou on 12-11-6.
+//  Copyright (c) 2012年 Douban Inc. All rights reserved.
+//
+
+#import "DOUObjectArray.h"
+
+@interface DOUNotificationArray : DOUObjectArray
+
+- (NSInteger)num;
+
+@end
